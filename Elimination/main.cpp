965b50eac0e7c1b0f3f0227f75ffaeebb892e37f
Ju Hyung Lee <jufoot@icloud.com>
2015-08-17 18:29:20 +0900

    Added Gauss-Jordan elimination

--- Elimination/main.cpp
+++ Elimination/main.cpp
@@ -46,15 +46,19 @@ bool GaussElimination(const MatrixXf &a, const VectorXf &b, MatrixXf &u, VectorX
 			std::swap(c(i), c(pivot_index));
 		}
 
+		float invPivot = 1.0f / pivot;
+
+		// elimination process
 		for (int r = i + 1; r < a.rows(); r++) {
-			// scaler for subtract a row
-			float scaler = u(r, i) / pivot;
+			// scalar for subtract a row
+			float scalar = u(r, i) * invPivot;
 
 			// subtract a row from scaled pivot row
 			for (int c = i; c < a.cols(); c++) {
-				u(r, c) -= scaler * u(i, c);
+				u(r, c) -= scalar * u(i, c);
 			}
-			c(r) -= scaler * c(i);
+			// subtract augmented part too
+			c(r) -= scalar * c(i);
 		}
 	}
 
@@ -92,6 +96,67 @@ bool SolveGaussElimination(const MatrixXf &a, const VectorXf &b, VectorXf &x) {
 	return true;
 }
 
+// Gauss-Jordan elimination
+// [ A | I ] = [ I | A^(-1) ]
+bool GaussJordanElimination(const MatrixXf &a, MatrixXf &ia) {
+	// 'a' should be a square matrix
+	assert(a.rows() == a.cols());
+	// 'ia' have the same size
+	assert(a.rows() == ia.rows() && a.cols() == ia.cols());
+
+	MatrixXf e = a;
+
+	ia = MatrixXf::Identity(a.rows(), a.cols());
+
+	// 'i' is diagonal index of the matrix 
+	for (int i = 0; i < a.rows(); i++) {
+		float pivot = e(i, i);
+		if (pivot == 0.0f) {
+			// abandon elimination if pivot is not exist
+			return false;
+		}
+
+		float invPivot = 1.0f / pivot;
+
+		// forward elimination process
+		for (int r = i + 1; r < a.rows(); r++) {
+			// scalar for subtract a row
+			float scalar = e(r, i) * invPivot;
+
+			// subtract a row from scaled pivot row
+			for (int c = i; c < a.cols(); c++) {
+				e(r, c) -= scalar * e(i, c);
+			}
+			// subtract augmented part too
+			ia.row(r) -= scalar * ia.row(i);
+		}
+	}
+
+	// 'i' is diagonal index of the matrix 
+	for (int i = a.rows() - 1; i >= 0; i--) {
+		float pivot = e(i, i);
+		float invPivot = 1.0f / pivot;
+
+		// backward elimination process
+		for (int r = i - 1; r >= 0; r--) {
+			// scalar for subtract a row
+			float scalar = e(r, i) * invPivot;
+
+			// subtract a row from scaled pivot row
+			e(r, i) -= scalar * e(i, i);
+			
+			// subtract augmented part too
+			ia.row(r) -= scalar * ia.row(i);
+		}
+
+		// make diagonal element to '1'
+		//e(i, i) *= invPivot;
+		ia.row(i) *= invPivot;
+	}
+
+	return true;
+}
+
 void TestGaussElimination() {
 	const int DIMENSION = 4;
 
@@ -103,7 +168,6 @@ void TestGaussElimination() {
 
 	VectorXf x;
 	bool invertible = ::SolveGaussElimination(a, b, x);
-
 	if (!invertible) {
 		std::cout << "ERROR: A is not invertible !!\n";
 		return;
@@ -123,10 +187,31 @@ void TestGaussElimination() {
 	std::cout << x << "\n\n";
 }
 
+void TestGaussJordanElimination() {
+	const int DIMENSION = 4;
+
+	MatrixXf a(DIMENSION, DIMENSION);
+	a << MatrixXf::Random(DIMENSION, DIMENSION);
+
+	MatrixXf ia(DIMENSION, DIMENSION);
+	bool invertible = ::GaussJordanElimination(a, ia);
+	if (!invertible) {
+		std::cout << "ERROR: A is not invertible !!\n";
+	}
+
+	std::cout << "A =\n";
+	std::cout << a << "\n\n";
+
+	std::cout << "A(^-1) =\n" << ia << "\n\n";
+	std::cout << "A(^-1) =\n" << a.inverse() << "\n\n";
+}
+
 int main() {
 	srand((unsigned int)time(0));
 
 	TestGaussElimination();
+
+	TestGaussJordanElimination();
 
 	getchar();
 
